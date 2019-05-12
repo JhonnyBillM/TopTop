4ef97a3c39cea475090d401351c1cfec34be5f1c
Jhonny Bill <jhonnybillm@gmail.com>
2019-05-12 07:23:03 -0400

    ADD - movie file.

---
+++ TopTop/TopTop/Model/Movie.swift
@@ -0,0 +1,23 @@
+//
+//  Movie.swift
+//  TopTop
+//
+//  Created by Oriontek iOS on 5/12/19.
+//  Copyright © 2019 com.jhonny.learning. All rights reserved.
+//
+
+import Foundation
+
+struct Movie: Codable {
+    var name: String
+    var rawReleaseDate: String = ""
+    var artworkUrl: URL
+    //var genres: [Genre]
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case rawReleaseDate = "releaseDate"
+        case artworkUrl = "artworkUrl100"
+        //case genres
+    }
+}
