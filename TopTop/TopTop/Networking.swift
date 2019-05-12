76afa0701358e1e1646e228a57813171a9c06426
Jhonny Bill <jhonnybillm@gmail.com>
2019-05-12 08:37:18 -0400

    ADD - playlists(completion:) method.

--- TopTop/TopTop/Networking.swift
+++ TopTop/TopTop/Networking.swift
@@ -36,4 +36,30 @@ struct Networking {
             }
         }.resume()
     }
+    
+    
+    /// Fetches the top 25 playlists from Apple Music.
+    ///
+    /// - Parameters:
+    ///     - result: Result type with playlist's array as it's success case and Swift.Error as it's failure case.
+    func playlists(completion: @escaping (_ result: Result<[Playlist], Error>) -> Void) {
+        let _url = "https://rss.itunes.apple.com/api/v1/us/apple-music/hot-tracks/all/25/explicit.json"
+        guard let url = URL(string: _url) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { return }
+            do {
+                guard let json = try JSONSerialization.jsonObject(with: data) as? [String : Any] else { return }
+                guard let feed = json["feed"] as? [String : Any] else { return }
+                guard let results = feed["results"] else { return }
+                
+                let resultsData = try JSONSerialization.data(withJSONObject: results)
+                let playlists = try JSONDecoder().decode([Playlist].self, from: resultsData)
+                completion(.success(playlists))
+            } catch let err {
+                print(err.localizedDescription)
+                completion(.failure(err))
+            }
+        }.resume()
+    }
 }
