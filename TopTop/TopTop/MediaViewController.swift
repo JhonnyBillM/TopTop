1ed514ad2832cb572456b9f9ea3b35c3d7fca1e9
Jhonny Bill <jhonnybillm@gmail.com>
2019-05-12 07:53:38 -0400

    ADD - View controller with navigation bar and segmented control.
    ADD - MediaViewController class.
    
    ADD - setUpView() and prepareSegmentedControl() methods.

--- TopTop/TopTop/MediaViewController.swift
+++ TopTop/TopTop/MediaViewController.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+import UIKit
+
+class MediaViewController: UIViewController {
+    
+    var navigationBar: UINavigationBar!
+    var customNavigationItem: UINavigationItem!
+    var segmentedControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        setUpView()
+    }
+    
+    private func setUpView() {
+        prepareSegmentedControl()
+        
+        navigationBar = UINavigationBar(frame: view.frame)
+        navigationBar.translatesAutoresizingMaskIntoConstraints = false
+        
+        customNavigationItem = UINavigationItem()
+        customNavigationItem.titleView = segmentedControl
+        
+        navigationBar.setItems([customNavigationItem], animated: true)
+        view.addSubview(navigationBar)
+        
+        let topConstraint = navigationBar.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor)
+        let leadingConstraint = navigationBar.leftAnchor.constraint(equalTo: view.leftAnchor)
+        let trailingConstraint = navigationBar.rightAnchor.constraint(equalTo: view.rightAnchor)
+        
+        NSLayoutConstraint.activate([
+            topConstraint,
+            leadingConstraint,
+            trailingConstraint
+        ])
+    }
+    
+    private func prepareSegmentedControl() {
+        segmentedControl = UISegmentedControl(frame: view.frame)
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+
+        segmentedControl.insertSegment(withTitle: "Movies", at: 0, animated: true)
+        segmentedControl.insertSegment(withTitle: "Playlists", at: 1, animated: true)
+        
+        segmentedControl.tintColor = .purple
+        segmentedControl.selectedSegmentIndex = 0
+    }
+}
